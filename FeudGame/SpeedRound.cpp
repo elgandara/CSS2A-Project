f2c10839d7bd18bfc8ccc754a03015416ca0d0ff
lauraa-c <chavez.lauraa7@gmail.com>
2015-05-08 13:30:12 -0700

    Updated game files.

---
+++ FeudGame/SpeedRound.cpp
@@ -0,0 +1,11 @@
+//***************************************************************
+// Filename.cpp
+// Author: Laura Chavez
+// Last Modified:
+// Description:
+//***************************************************************
+//
+
+#include "SpeedRound.h"
+using namespace std;
+
---
+++ FeudGame/SpeedRound.h
@@ -0,0 +1,29 @@
+#ifndef SPEEDROUND_H_INCLUDED
+#define SPEEDROUND_H_INCLUDED
+
+//**********************************************************************
+// Class Name: SpeedRound
+//
+// Description:
+//
+//**********************************************************************
+
+
+#include "Question.h"
+using namespace std;
+
+class SpeedRound
+{
+    private:
+        string questions[6];
+        int points;
+    public:
+        SpeedRound();
+
+
+};
+
+
+
+
+#endif // SPEEDROUND_H_INCLUDED
