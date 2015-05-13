fd02b706ae0ae1f2a43524bf32a8121e4b0a3fa0
Eliasar Gandara <eliazargandara@gmail.com>
2015-05-13 16:46:29 -0700

    Added methods and variables

--- FeudGame/FaceOff.cpp
+++ FeudGame/FaceOff.cpp
@@ -1,7 +1,63 @@
-//********************************************************
-//Title:
-//Description:
-//Date:
-//Version:
-//Compiler:
-//********************************************************
+//*********************************************************
+//Title:Faceoff.cpp
+//Description:File will contain the implementation for the
+//class called FaceOff
+//Date: 5/5/15
+//Version: 1
+//Compiler: CB 13.12
+//*********************************************************
+#include "FaceOff.h"
+#include "Question.h"
+#include <iostream>
+//******************************************************
+//Summary:Default constructor of the class
+//PreCondition: An object of the class must be created.
+//PostCondition:The values of the variables will be set.
+//
+//*******************************************************
+FaceOff::FaceOff()
+{
+    Question question.createNewQuestion();
+     answer = "";
+     chances = 0;
+     playerScore  = 0;
+     //Will call the void getAnswers(answers) method and set the answers for the array
+}
+//*************************************************
+//Summary:Function will initiate a FaceOff Round.
+//PreCondition:
+//PostCondition:
+//
+//*************************************************
+void FaceOff::runRound()
+{
+    int storeAnswer = 0;
+    cout << question.getQuestionNumber() << ")";
+    cout << question.getQuestion() << endl;
+    do
+    {
+        cin >> answer;
+        question.getScore(answer);
+        if (storeAnswer == 0)
+            chances++;
+            cout << chances << endl;
+
+    }while(chances < 3)
+
+}
+//***************************************************************
+//Summary:Function will display the answers of the FaceOff Round.
+//PreCondition:
+//PostCondition:
+//
+//***************************************************************
+FaceOff::display()
+{
+    cout << "==================================================="; << endl;
+    for(int i = 0; i < 8; i++)
+    {
+        cout << answers[i] << endl;
+    }
+
+    cout << "===================================================="; << endl;
+}
